c5fe159608d237bd6e0b68d2054b17e2a08b3fa3
Sebastian009w <ia3266595@gmail.com>
2023-03-26 12:28:27 -0500

    se estiliza gallery

--- src/components/GalleryUs.svelte
+++ src/components/GalleryUs.svelte
@@ -4,16 +4,38 @@
 </script>
 
 <div class="container-gallery">
-	<div>
+	<div class="title-gallery">
 		<Felt title="Gallery" />
 	</div>
-	<div>
+	<div class="image-gallery">
 		<Gallery image="https://i.postimg.cc/cLh2wJGw/Rectangle-6.png" title="image" />
+
 		<Gallery image="https://i.postimg.cc/vZYSYdxB/Rectangle-8.png" title="image" />
+
 		<Gallery image="https://i.postimg.cc/d3c4zMKJ/Rectangle-9.png" title="image" />
+
 		<Gallery image="https://i.postimg.cc/8kLyLChc/Rectangle-10.png" title="image" />
+
+		<Gallery image="https://i.postimg.cc/VNLXWGSS/Rectangle-11.png" title="image" />
+
 		<Gallery image="https://i.postimg.cc/4N5BFF2T/Rectangle-7.png" title="image" />
 	</div>
 </div>
 
-<style></style>
+<style>
+	.container-gallery {
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	.title-gallery {
+		text-align: center;
+		padding: 2.2rem;
+	}
+	.image-gallery {
+		display: flex;
+		justify-content: space-around;
+		flex-wrap: wrap;
+		align-items: center;
+		gap: 1rem 1rem;
+	}
+	
+</style>
